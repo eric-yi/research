c92495ccb6a7e985bb9468bee2774d57de774a88
Xiaobin Yi <xbyi@CNxbyi.local>
2019-03-17 20:38:53 +0800

    add knn r language

---
+++ prj/r/wbcd.r
@@ -0,0 +1,13 @@
+normlize <- function(x) { 
+	return ((x - min(x)) / (max(x) - min(x)) ) 
+}
+
+wdbc = read.csv("../../dataset/WDBC/WDBC.dat")
+wdbc <- wdbc[2:32]
+wdbc_n <- as.data.frame(lapply(wdbc[2:31], normlize))
+wdbc_train <- wdbc_n[1:469, ]
+wdbc_test <- wdbc_n[470:568, ]
+wdbc_train_labels <- wdbc_n[1:469, 1]
+wdbc_test_labels <- wdbc_n[470:569, 1]
+library("class")
+wdbc_pred <- knn(train=wdbc_train, test=wdbc_test, cl=wdbc_train_labels, k=3)
