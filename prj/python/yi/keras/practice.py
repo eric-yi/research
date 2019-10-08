ee98a5a5e500a2e3801cdb28ce2ee1b05c2c0aa1
xiaobin yi <yi_xiaobin@163.com>
2019-10-08 09:25:11 +0800

    refactor: rename function name of practice

--- prj/python/yi/keras/practice.py
+++ prj/python/yi/keras/practice.py
@@ -1,18 +1,54 @@
 #!/usr/bin/evn python
 # -*- coding:utf-8 -*-
 import logging
+import numpy as np
+import keras
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def do_lab_1():
+def do_lab_with_softmax():
+    x_train = np.random.random((1000, 20))
+    y_train = keras.utils.to_categorical(np.random.randint(10, size=(1000, 1)), num_classes=10)
+    x_test = np.random.random((100, 20))
+    y_test = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
     model = Sequential()
-    model.add(Dense(units=64, input_dim=100))
-    model.add(Activation("relu"))
-    model.add(Dense(units=10))
-    model.add(Activation("softmax"))
-    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+    model.add(Dense(64, activation='relu', input_dim=20))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=sgd,
+                  metrics=['accuracy'])
+    model.fit(x_train, y_train,
+              epochs=20,
+              batch_size=128)
+    score = model.evaluate(x_test, y_test, batch_size=128)
+    logger.info(f'do lab with softmax score: {score}')
 
+
+def do_lab_with_mlp():
+    x_train = np.random.random((1000, 20))
+    y_train = np.random.randint(2, size=(1000, 1))
+    x_test = np.random.random((100, 20))
+    y_test = np.random.randint(2, size=(100, 1))
+    model = Sequential()
+    model.add(Dense(64, input_dim=20, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+    model.fit(x_train, y_train,
+              epochs=20,
+              batch_size=128)
+    score = model.evaluate(x_test, y_test, batch_size=128)
+    logger.info(f'do lab with mlp score: {score}')
--- prj/python/yi/tests/keras/test_practice.py
+++ prj/python/yi/tests/keras/test_practice.py
@@ -4,6 +4,10 @@ import yi.keras.practice as practice
 
 
 class TestSimple:
-    def test_do_lab_1(self):
-        practice.do_lab_1()
+    def test_do_lab_with_softmax(self):
+        practice.do_lab_with_softmax()
+        assert True
+
+    def test_do_lab_with_mlp(self):
+        practice.do_lab_with_mlp()
         assert True
